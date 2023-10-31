df46ea9b2c2260acc1eb715311522fc65b52eefd
eduardo <educonts08@gmail.com>
2023-10-31 20:05:52 -0300

    first commit

---
+++ src/http/routes.ts
@@ -0,0 +1,7 @@
+import { FastifyInstance } from "fastify";
+import { Register } from "./controllers/register";
+
+export async function appRoutes(app: FastifyInstance) {
+  app.post('/users', Register)
+
+}
